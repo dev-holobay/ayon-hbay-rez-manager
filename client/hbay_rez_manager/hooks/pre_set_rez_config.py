7cbbd83d546761dcf84ee470988f02e1ceccca43
nebukadhezer <johannes.hezer@gmail.com>
2026-01-15 10:07:35 +0100

    update and add hooks for config and env variable injection

---
+++ client/hbay_rez_manager/hooks/pre_set_rez_config.py
@@ -0,0 +1,24 @@
+from ayon_applications import PreLaunchHook, LaunchTypes, manager
+
+
+class PreLaunchSetRezConfig(PreLaunchHook):
+    """Injects Rez config environment variables before DCC launch."""
+
+    order = -100  # this hook needs to execute first as it sets the rez config environment variables
+    platforms = {"windows"}
+    launch_types = {LaunchTypes.local}
+
+    def execute(self):
+        # Access the addon settings
+        self.log.info("Setting Rez Config Environment Variables")
+        self.log.info(self.launch_context.data.get("project_settings").get(
+            "hbay_rez_manager"))
+        rez_settings = self.launch_context.data.get("project_settings", {}).get(
+            "hbay_rez_manager", {}).get("rez_config_options", {})
+        rez_path = rez_settings.get("rez_packages_path_win")
+        if not rez_path:
+            self.log.warning("REZ_PACKAGES_PATH is not set in AYON settings.")
+            return
+
+        self.launch_context.env.update({"REZ_PACKAGES_PATH": rez_path})
+        self.log.info(f"Rez Environment Set: REZ_PACKAGES_PATH={rez_path}")
---
+++ client/hbay_rez_manager/hooks/pre_set_rez_env.py
@@ -0,0 +1,105 @@
+import os
+import subprocess
+import json
+
+from ayon_applications import PreLaunchHook, ApplicationLaunchFailed, \
+    LaunchTypes
+
+from ayon_core.lib import vendor_bin_utils
+from ayon_applications.defs import ApplicationExecutable
+
+
+class PreLaunchSetRezEnv(PreLaunchHook):
+    """Add Rez packages to the launch environment.
+
+    It will merge the parent launch context environment with a rez resolved
+    environment defined by the `AYON_REZ_PACKAGES` environment variable.
+
+    The `AYON_REZ_PACKAGES` variable should contain a list of rez package
+    names separated by the OS path separator (os.pathsep). This can be
+    set in AYON in e.g. application environments or tool environments to build
+    up a list of packages to resolve with.
+
+    Note that the rez resolved environment will be resolved with all parent
+    variables enabled and will merge into the launch context environment.
+    """
+    order = -99
+    platforms = {"windows"}
+    launch_types = {LaunchTypes.local}
+
+    def execute(self):
+        ayon_rez_packages = self.launch_context.env.get("AYON_REZ_PACKAGES")
+        self.log.info(f"AYON_REZ_PACKAGES: {ayon_rez_packages}")
+        if not ayon_rez_packages:
+            return
+
+        packages: list[str] = ayon_rez_packages.split(os.pathsep)
+        python_cmd = (
+            "import json;"
+            "from rez.resolved_context import ResolvedContext;"
+            f"print(json.dumps(ResolvedContext({repr(packages)}).get_environ()))"
+        )
+
+        # We assume `rez` is available on PATH as command-line and has the rez
+        # python available with rez python library so we can resolve the env
+        # easily to JSON and merge it into the launch context environment.
+        # TODO: If the current environment would have `rez` python package
+        #  available then we could avoid the subprocess call here and resolve
+        #  directly within current Python process.
+
+        command = [
+            "rez",
+            "python",
+            "-c",
+            python_cmd
+        ]
+
+        # Enforce upstream environment to be included so that it includes the
+        # parent AYON environment completely
+        tmp_env = self.launch_context.env.copy()
+        tmp_env["REZ_ALL_PARENT_VARIABLES"] = "1"
+
+        # Get the rez resolved enviroment
+        result = subprocess.run(
+            command,
+            env=tmp_env,
+            capture_output=True,
+        )
+        if result.returncode != 0:
+            output = ""
+            if result.stdout:
+                output += result.stdout.decode("utf-8")
+            if result.stderr:
+                output += result.stderr.decode("utf-8")
+
+            rez_packages = " ".join(packages)
+            self.log.error(output)
+
+            # Assume we can parse last line as traceback message
+            message = output.splitlines()[-1].split(":", 1)[-1].strip()
+            raise ApplicationLaunchFailed(
+                f"Rez environment resolution failed for packages: {rez_packages}."
+                f"\n\n{message}"
+            )
+        # self.log.info(result.stdout.decode("utf-8"))
+        # Update self.launch_context.env with the rez environment
+        rez_env: dict[str, str] = json.loads(result.stdout)
+
+        self.launch_context.env.update(rez_env)
+
+        # filter paths for duplicates
+        paths = self.launch_context.env.get("PATH", "").split(os.pathsep)
+        # dict.fromkeys is a fast way to get unique items in order
+        unique_paths = list(dict.fromkeys(p for p in paths if p))
+        self.launch_context.env["PATH"] = os.pathsep.join(unique_paths)
+
+        for k in sorted(self.launch_context.env.keys()):
+            v = self.launch_context.env[k]
+            self.log.info(f"{k}={v}")
+
+        # patch the executable in launch_context so later executed prelaunch hooks continue to function
+        executable = vendor_bin_utils.find_executable(
+            str(self.launch_context.executable),
+            optional_paths=self.launch_context.env.get("PATH").split(
+                os.pathsep))
+        self.launch_context.executable = ApplicationExecutable(executable)
