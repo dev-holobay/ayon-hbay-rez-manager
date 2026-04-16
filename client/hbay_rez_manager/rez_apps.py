27fc58d901152afa4ecbeda2f49bbb969ad72bfb
Johannes Hezer <johannes.hezer@gmail.com>
2026-04-16 13:02:14 +0200

    add f3d to rez apps

--- client/hbay_rez_manager/rez_apps.py
+++ client/hbay_rez_manager/rez_apps.py
@@ -1,6 +1,7 @@
+"""Configurations for Rez applications that can be launched
+from the tray menu.
 """
-Configuration for Rez applications that can be launched from the tray menu.
-"""
+
 import os
 
 ADDON_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -8,17 +9,30 @@ ADDON_ROOT = os.path.dirname(os.path.abspath(__file__))
 REZ_APPS = {
     "USD View": {
         "rez-request": ["usd_nvidia"],
-        "rez-executable": {"windows":"usdview_gui.bat", "linux":"usdview_gui", "darwin":"usdview_gui"},
-        "icon": os.path.join(ADDON_ROOT, "icons", "usd.png")
+        "rez-executable": {
+            "windows": "usdview_gui.bat",
+            "linux": "usdview_gui",
+            "darwin": "usdview_gui",
+        },
+        "icon": os.path.join(ADDON_ROOT, "icons", "usd.png"),
     },
     "Open Rv": {
         "rez-request": ["openrv"],
-        "rez-executable": {"windows":"rv.exe", "linux":"rv", "darwin":"rv"},
-        "icon": os.path.join(ADDON_ROOT, "icons", "rv.png")
+        "rez-executable": {"windows": "rv.exe", "linux": "rv", "darwin": "rv"},
+        "icon": os.path.join(ADDON_ROOT, "icons", "rv.png"),
     },
     "QuiltiX": {
         "rez-request": ["QuiltiX"],
-        "rez-executable": {"windows": "QuiltiX.bat", "linux": "", "darwin": ""},
-        "icon": os.path.join(ADDON_ROOT, "icons", "quiltix.png")
+        "rez-executable": {
+            "windows": "QuiltiX.bat",
+            "linux": "",
+            "darwin": "",
+        },
+        "icon": os.path.join(ADDON_ROOT, "icons", "quiltix.png"),
+    },
+    "F3d": {
+        "rez-request": ["f3d"],
+        "rez-executable": {"windows": "f3d", "linux": "", "darwin": ""},
+        "icon": os.path.join(ADDON_ROOT, "icons", "f3d.png"),
     },
 }
